923efac65fab05a9a1df64a56c0699d849c75cac
Hrithik Garg <63644595+hrithikgarg@users.noreply.github.com>
2021-06-12 20:06:00 +0530

    Update game.js

--- game.js
+++ game.js
@@ -26,24 +26,49 @@ function nextSequence(){
     //play audio
     makeSound(randomChosenColour);
 }
-
 $(".btn").click(function(){
+        
+    if(isCalled == false){
+        //extract id
+        var userChosenColor = $(this).attr("id");
+        //store pattern on click
+        userClickedPattern.push(userChosenColor);
+        // console.log(userClickedPattern);
 
-    //extract id
-    var userChosenColor = $(this).attr("id");
-    //store pattern on click
-    userClickedPattern.push(userChosenColor);
-    // console.log(userClickedPattern);
+        //make sound
+        makeSound(userChosenColor);
+        animate(userChosenColor);
 
-    //make sound
-    makeSound(userChosenColor);
-    animate(userChosenColor);
-
-    checkAnswer(userClickedPattern.length-1);
+        checkAnswer(userClickedPattern.length-1);
+    }
 });
 
+
+
+function checkAnswer(currentLevel){
+   if(userClickedPattern[currentLevel] !== gamePattern[currentLevel]){
+
+       makeSound("wrong");
+       
+       $("body").addClass("game-over");
+       setTimeout(function(){
+            $("body").removeClass("game-over");
+        },'200');
+        $("#start").text("😭Game Over😭"+"Click Me to Restart");
+
+        isCalled = true;
+        Level = 0;
+        gamePattern = [];
+        gameStarted = false; 
+   }
+   else if(gamePattern.length === userClickedPattern.length){
+      setTimeout(() => {nextSequence();
+    }, 1000);
+   }
+}
+
 function makeSound(onClickColor){
-    var audio = new Audio(onClickColor + ".mp3");
+    var audio = new Audio("sounds/" + onClickColor + ".mp3");
     audio.play();   
 }
 
@@ -54,33 +79,14 @@ function animate(color){
         button.removeClass("pressed");
     },100);
 }
-function checkAnswer(currentLevel){
-   if(userClickedPattern[currentLevel] !== gamePattern[currentLevel]){
 
-       makeSound("wrong");
-       
-       $("body").addClass("game-over");
-       setTimeout(function(){
-            $("body").removeClass("game-over");
-        },'200');
-        $("#level-title").text("Game Over, Press Any Key to Restart");
-
-        isCalled = true;
-        Level = 0;
-        gamePattern = [];
-   }
-   else if(gamePattern.length === userClickedPattern.length){
-      setTimeout(() => {nextSequence();
-    }, 1000);
-   }
-
-}
 //call the function when user press any key
 var isCalled = true;
-$("body").keypress(function(){
+$("#start").click(function(){
     if(isCalled){
+        $("#start").text("😁Click me to start😁");
         nextSequence();
-        isCalled = false;
+        isCalled = false;   
     }
 });
 
